6d4f59653893e223ab493380bdde747e27c5be29
Mohamadreza Irick <mohamadresaaa@gmail.com>
2026-06-28 23:28:20 +0330

    Update watermark.py

--- services/watermark.py
+++ services/watermark.py
@@ -1,4 +1,18 @@
+import os
+import requests
 from PIL import Image, ImageDraw, ImageFont
+
+FONT_URL = "https://github.com/google/fonts/raw/main/apache/roboto/Roboto-Bold.ttf"
+FONT_PATH = "Roboto-Bold.ttf"
+
+def get_font(size):
+    
+    if not os.path.exists(FONT_PATH):
+        r = requests.get(FONT_URL)
+        with open(FONT_PATH, "wb") as f:
+            f.write(r.content)
+
+    return ImageFont.truetype(FONT_PATH, size)
 
 
 def add_watermark(image_path, text="@LockScreenZone"):
@@ -6,16 +20,11 @@ def add_watermark(image_path, text="@LockScreenZone"):
 
     overlay = Image.new("RGBA", image.size, (0, 0, 0, 0))
     draw = ImageDraw.Draw(overlay)
+  ‌
+    font_size = int(image.width * 0.25) 
 
-    # 🔥 فونت بزرگ
-    font_size = max(200, image.width // 5)
+    font = get_font(font_size)
 
-    try:
-        font = ImageFont.truetype("DejaVuSans.ttf", font_size)
-    except OSError:
-        font = ImageFont.load_default()
-
-    # 📍 محاسبه مرکز متن
     bbox = draw.textbbox((0, 0), text, font=font)
     text_w = bbox[2] - bbox[0]
     text_h = bbox[3] - bbox[1]
@@ -23,9 +32,6 @@ def add_watermark(image_path, text="@LockScreenZone"):
     x = (image.width - text_w) // 2
     y = (image.height - text_h) // 2
 
-    # ⚪ متن سفید ساده
     draw.text((x, y), text, font=font, fill=(255, 255, 255, 255))
 
-    result = Image.alpha_composite(image, overlay)
-
-    return result.convert("RGB")
+    return Image.alpha_composite(image, overlay).convert("RGB")
