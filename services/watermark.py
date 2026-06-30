949647a18d52c09500ed1d65d0c796de3d99f42c
l1mindr <mohamadresaaa@gmail.com>
2026-06-30 11:55:48 +0330

    refactor: streamline font loading and logging in watermark function

--- services/watermark.py
+++ services/watermark.py
@@ -1,30 +1,32 @@
-import os
-import requests
+import logging
+
 from PIL import Image, ImageDraw, ImageFont
 
-FONT_URL = "https://github.com/google/fonts/raw/main/apache/roboto/Roboto-Bold.ttf"
-FONT_PATH = "Roboto-Bold.ttf"
-
-def get_font(size):
-    
-    if not os.path.exists(FONT_PATH):
-        r = requests.get(FONT_URL)
-        with open(FONT_PATH, "wb") as f:
-            f.write(r.content)
-
-    return ImageFont.truetype(FONT_PATH, size)
-
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
 
 def add_watermark(image_path, text="@LockScreenZone"):
+    logger = logging.getLogger("watermark")
+
     image = Image.open(image_path).convert("RGBA")
 
     overlay = Image.new("RGBA", image.size, (0, 0, 0, 0))
     draw = ImageDraw.Draw(overlay)
-  ‌
-    font_size = int(image.width * 0.25) 
 
-    font = get_font(font_size)
+    font_size = 20
 
+    try:
+        font = ImageFont.truetype(
+            "/System/Library/Fonts/Supplemental/Arial.ttf", font_size
+        )
+        logger.info("Font load successfully")
+    except OSError as e:
+        logger.error(f"Font loading error: {e}")
+        font = ImageFont.load_default()
+
+    # 📍 محاسبه مرکز متن
     bbox = draw.textbbox((0, 0), text, font=font)
     text_w = bbox[2] - bbox[0]
     text_h = bbox[3] - bbox[1]
@@ -32,6 +34,9 @@ def add_watermark(image_path, text="@LockScreenZone"):
     x = (image.width - text_w) // 2
     y = (image.height - text_h) // 2
 
+    # ⚪ متن سفید ساده
     draw.text((x, y), text, font=font, fill=(255, 255, 255, 255))
 
-    return Image.alpha_composite(image, overlay).convert("RGB")
+    result = Image.alpha_composite(image, overlay)
+
+    return result.convert("RGB")
